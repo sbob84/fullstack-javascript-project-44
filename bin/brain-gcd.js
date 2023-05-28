eb3f00e4ce091787b9fc97d5c5825cddfa0e477d
sbob <sergebobkov@yahoo.com>
2023-05-28 16:21:28 +0000

    Step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import * as cli from '../src/cli.js';
+
+function GCD(a, b) {
+  if (!b) {
+    return a;
+  }
+  return GCD(b, a % b);
+}
+
+const user = cli.GetNameAndGreet('Find the greatest common divisor of given numbers.');
+
+let j = 0;
+for (let i = 0; i < 3; i++) {
+  const valA = Math.floor(Math.random() * 100);
+  const valB = Math.floor(Math.random() * 100);
+  const result = GCD(valA, valB);
+  const answer = Number(cli.GetAnswer(`Question: ${valA} ${valB}\nYour answer: `));
+  if (cli.Check(answer === result, answer, result, user)) {
+    j++;
+  } else {
+    break;
+  }
+}
+
+if (j === 3) {
+  console.log(`Congratulations, ${user}!`);
+}
