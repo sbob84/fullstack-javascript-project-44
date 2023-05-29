f1cd39db4d8acff1de6395db2575d916baca2ec1
sbob <sergebobkov@yahoo.com>
2023-05-29 13:59:45 +0000

    >:|

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,7 +7,7 @@ const user = cli.GetNameAndGreet('Answer "yes" if the number is even, otherwise 
 let j = 0;
 for (let i = 0; i < 3; i += 1) {
   const number = Math.floor(Math.random() * 100);
-  const result = number % 2 == 1 ? 'no' : 'yes';
+  const result = number % 2 === 1 ? 'no' : 'yes';
   const answer = cli.GetAnswer(`Question: ${number}\nYour answer: `);
   const check = answer === 'yes' ? number % 2 === 0 : number % 2 === 1;
 
